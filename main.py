8dcf34d23e67b219332f333dab6873c0054687f5
Felipe Pontes <felipemfpontes@gmail.com>
2017-01-08 23:59:28 -0300

    Rewrite main.py

--- main.py
+++ main.py
@@ -1,52 +1,55 @@
-import requests
-import sys
+from __future__ import print_function
 from os import system
+from sys import platform
+import argparse
+import requests
 
-def Plataforma():
-    if sys.platform != "linux2":
-        system("cls")
+
+def clear_screen():
+    if platform.startswith('linux'):
+        system('clear')
     else:
-        system("clear")
-                           
-def Banner():
-    print """
+        system('cls')
 
-  _____           _ _      
- |  __ \         | (_)     
- | |__) |   _  __| |_ _ __ 
+
+def print_banner():
+    print("""
+
+  _____           _ _
+ |  __ \         | (_)
+ | |__) |   _  __| |_ _ __
  |  ___/ | | |/ _` | | '__|
- | |   | |_| | (_| | | |   
- |_|    \__, |\__,_|_|_|   
-         __/ |             
-        |___/              
+ | |   | |_| | (_| | | |
+ |_|    \__, |\__,_|_|_|
+         __/ |
+        |___/
 
 [+]Github:   github.com/nash0x27
 [+]Email:    gabrieldutra-08@protonmail.com
 [+]Facebook: fb.com/n4shxx
 
 =============================================
-Uso:
-    Alvo: site.com.br
 
-=============================================
-"""                                           
-                                            
-def Main():
-    wordlist1 = open('wordlist/wordlist.txt')
-    diretorios = wordlist1.readlines()
-    
-    alvo = raw_input("Alvo: ")
-    url = 'http://'+alvo+'/'
+""")
 
-    for diretorio in diretorios:
-        error = 404
 
-        if diretorio[0] != "#":
-            resposta = requests.get(url+diretorio)
-            error = resposta.status_code
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('target', help='target to explore (e.g.: google.com)')
+    args = parser.parse_args()
 
-        if diretorio != 404:
-            print url+diretorio, error
-Plataforma()
-Banner()
-Main()
+    with open('wordlist/wordlist.txt') as wordlist1:
+        endpoints = wordlist1.readlines()
+        url_base = args.target if args.target.startswith('http') else 'http://' + args.target
+        for endpoint in endpoints:
+            error = 404
+            url = '{}/{}'.format(url_base, endpoint).strip()
+            if endpoint[0] != "#":
+                response = requests.get(url)
+                error = response.status_code
+            print('[{1}] {0}'.format(url, error))
+
+if __name__ == '__main__':
+    clear_screen()
+    print_banner()
+    main()
